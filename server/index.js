d1f068c9064e26d23e3f34b0d60b3067ac82ea4e
Ploychompu-Kaewgool <Ploychompu.ka@ku.th>
2025-02-17 16:27:00 +0700

    edit git

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise');
 const app = express();
 
 app.use(bodyParser.json());
@@ -7,24 +8,54 @@ const port = 8000;
 
 // เก็บ user ที่ส่งมาจาก client
 let users = [];
-let counter = 1
 
+let conn = null;
+
+/*
+GET /users สำหรับ get ข้อมูล user ทั้งหมด
+POST /user สำหรับสร้าง user ใหม่บันทึกเข้าไป
+GET /user/:id สำหรับ get ข้อมูล user รายคนที่ต้องการ
+*/
+const initmysql = async () => {
+    conn = await mysql.createConnection({
+        host: 'localhost',
+        user:'root',
+        password: 'root',
+        database: 'webdb',
+        port: 8830
+    })
+}
+
+
+/*app.get('/testdb-new', async (req, res) => {
+
+    try {
+        const results = await conn.query('SELECT * FROM users')
+        res.json(results[0]) // ส่งข้อมูลกลับไปที่ results ในรูปแบบ json
+    } catch (error) { 
+        console.log('Erro fetching users:', error.message)
+        res.status(500).json({error:'Error fetching users'})
+        
+    }
+    
+})*/
 
 // path = GET /users
-app.get('/users', (req, res) => {
-    res.json(users);
+app.get('/users', async (req, res) => {
+    const results = await conn.query('SELECT * FROM users')
+    res.json(results[0]);
 });
 
 // path = POST /user
-app.post('/user', (req, res) => {
+app.post('/users', async (req, res) => {
     let user = req.body;
-    user.id = counter;
-    counter += 1;
-    users.push(user);
+    const results = await conn.query('INSERT INTO users SET ?', user)
+    console.log('results:', results)
     res.json({
-        message : "User created",        
-        user : user
+        message : "User created",
+        data: results[0]
     });
+    
 });
 
 // path = PUT /user/:id
@@ -65,7 +96,8 @@ app.delete('/user/:id', (req, res) => {
     });
 });
 
-app.listen(port, (req, res) => {
+app.listen(port, async(req, res) => {
+    await initmysql();
     console.log('Server is running on port' + port);
 });
 
